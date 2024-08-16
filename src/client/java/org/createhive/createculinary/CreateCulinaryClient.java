4154c16c94516de38d0f63548e119141c83d34c3
mayreader <120708880+mayreader@users.noreply.github.com>
2024-08-16 22:13:54 +1000

    Initial Code Upload

---
+++ src/client/java/org/createhive/createculinary/CreateCulinaryClient.java
@@ -0,0 +1,10 @@
+package org.createhive.createculinary;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class CreateCulinaryClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
