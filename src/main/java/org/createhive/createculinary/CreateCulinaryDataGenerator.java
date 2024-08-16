b01ed1bac7755c345e0f9f0aac0800c8b7629905
mayreader <120708880+mayreader@users.noreply.github.com>
2024-08-16 22:27:45 +1000

    Initial Code Commit (round 2)

---
+++ src/main/java/org/createhive/createculinary/CreateCulinaryDataGenerator.java
@@ -0,0 +1,11 @@
+package org.createhive.createculinary;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class CreateCulinaryDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
